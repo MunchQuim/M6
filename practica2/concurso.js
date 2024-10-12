ed720edd44c5e6b48638cfe485959ccb70c62cbb
Quim <qpinedasotes@gmail.com>
2024-10-12 22:25:50 +0200

    casi terminado

--- practica2/concurso.js
+++ practica2/concurso.js
@@ -1,9 +1,13 @@
+
 let nombre;
 let ronda = 0;
 let rondaMax = 0;
 //recoger datos del quiz
 let listadoQuiz = [];
+let listadoPostQuiz = [];
 let listadoDivQuiz = [];
+let correctas = 0;
+let incorrectas = 0;
 async function prepararPreguntas() {
     let response = await fetch("https://the-trivia-api.com/api/questions?limit=10&categories=general_knowledge");
     listadoQuiz = await response.json();
@@ -11,7 +15,7 @@ async function prepararPreguntas() {
     let contadorPreguntas = 0;
     let contadorRondas = 0;
     listadoQuiz.forEach(element => {
-        
+
         let containerQuestion = document.createElement('div');
         containerQuestion.id = "ContainerQuestion" + contadorRondas;
 
@@ -30,7 +34,7 @@ async function prepararPreguntas() {
 
             let respuesta = document.createElement('input');
             respuesta.type = "radio";
-            respuesta.name = contadorPreguntas + "";
+            respuesta.name = element['id'];
             respuesta.id = "radio" + contadorPreguntas + posicion;
 
             let respuestaTexto = document.createElement('span');
@@ -46,7 +50,7 @@ async function prepararPreguntas() {
 
         let respuesta = document.createElement('input');
         respuesta.type = "radio";
-        respuesta.name = contadorPreguntas + "";
+        respuesta.name = element['id'];
         respuesta.id = "radio" + contadorPreguntas + 4;
 
         let respuestaTexto = document.createElement('span');
@@ -71,30 +75,30 @@ async function prepararPreguntas() {
             contadorRondas += 1;
         }
     });
-    rondaMax = contadorRondas;
+    rondaMax = contadorRondas - 1;
 
-    for (let index = 0; index < listadoDivQuiz.length; index+= 2) {
+    for (let index = 0; index < listadoDivQuiz.length; index += 2) {
         let containerRonda = document.createElement("div");
-        containerRonda.className = "ronda "+(index/2);
-        if(index >0){
+        containerRonda.className = "ronda " + "ronda" + (index / 2);
+        if (index > 0) {
             containerRonda.style.display = "none";
         }
         containerRonda.appendChild(listadoDivQuiz[index]);
-        containerRonda.appendChild(listadoDivQuiz[index+1]);
+        containerRonda.appendChild(listadoDivQuiz[index + 1]);
         document.getElementById("containerQuiz").appendChild(containerRonda);
     }
-/*     for (let index = 1; index < rondaMax; index++) {
-        let containersQuestion = document.querySelectorAll("#ContainerQuestion" + index);
-        containersQuestion.forEach(element => {
-            element.style.display = "none";
-        });
-    } */
+    /*     for (let index = 1; index < rondaMax; index++) {
+            let containersQuestion = document.querySelectorAll("#ContainerQuestion" + index);
+            containersQuestion.forEach(element => {
+                element.style.display = "none";
+            });
+        } */
+
+    return listadoQuiz;
 
 }
+/* listadoPostQuiz = prepararPreguntas(); */
 prepararPreguntas();
-
-
-
 
 document.getElementById("btnName").addEventListener("click", guardarNombre, false);
 function guardarNombre() {
@@ -102,6 +106,75 @@ function guardarNombre() {
         nombre = document.getElementById("fname").value;
         document.getElementById("containerName").style.display = "none";
         document.getElementById("containerQuiz").style.display = "flex";
+        document.getElementById("corregir").style.display = "flex";
     }
 }
 
+document.getElementById("corregir").addEventListener("click", corregir, false);
+
+function corregir() {
+    //mire si he acertado las preguntas de la ronda actual
+    let radioNameArray = document.querySelectorAll('input[type="radio"]:checked:not([disabled])');
+    radioNameArray.forEach(radio => {
+
+        let radioName = radio.name;
+        listadoQuiz.forEach(element => {
+            if (element['id'] == radioName) {
+                // me cambie el color segun acierte o no
+                if (radio.nextElementSibling.textContent == element['correctAnswer']) {
+                    radio.nextElementSibling.style.color = "green";
+                    correctas += 1;
+                } else {
+                    radio.nextElementSibling.style.color = "red";
+                    incorrectas -= 1;
+                }
+            }
+        });
+        //me bloquee los botones de la ronda actual
+        document.getElementsByName(radioName).forEach(element => {
+            element.disabled = true;
+        });
+    });
+    if (correctas % 2 == 0 && correctas > 0) {
+        pasarRonda();
+    }
+    else {
+        perder();
+    }
+
+
+    //si he acertado todas paso de ronda, si no pierdo
+}
+function pasarRonda() {
+
+    ronda += 1;
+    console.log("pasando a ronda " + ronda + " de " + rondaMax);
+    if (ronda <= rondaMax) {
+        document.querySelectorAll('.ronda' + ronda)[0].style.display = "flex";
+    } else {
+        ganar();
+    }
+
+}
+
+
+function perder() {
+
+    document.getElementById("gameOver").style.display = "block";
+    guardarDatos(nombre, correctas, false);
+}
+
+function ganar() {
+    document.getElementById("mainContent").style.display = "none";
+    document.getElementById("congratulations").style.display = "block"; ç
+    guardarDatos(nombre, correctas, true);
+}
+
+// Función para guardar datos en el archivo JSON
+function guardarDatos(pNombre, pCorrectas, pGanado) {
+    let nuevoDato = {
+        nombre: pNombre,
+        correctas: pCorrectas,
+        haGanado: pGanado
+    };
+}
